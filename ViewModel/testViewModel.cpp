b7a1253e463bade1c313e0ef60c2504793eab5e7
ZhuYuanMing <3170102994@zju.edu.cn>
2019-07-14 20:55:36 +0800

    ?

--- ViewModel/testViewModel.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by px on 7/12/2019.
-//
-
-#include "./ViewModel.h"
-int main(){
-    printf("test");
-}
--- model/Model.cpp
+++
@@ -1,6 +0,0 @@
-#include "./Model.h"
-
-std::shared_ptr<EditableVideo> Model::openFile(const std::string path) {
-    currentVideo = std::make_shared<EditableVideo>(path);
-    return currentVideo;
-}
